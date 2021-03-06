f2ae25a9f3294273582212dc363ef86f775ebf06
adsonatural <adsonhenriquesilva@gmail.com>
2021-03-06 11:37:12 +0100

    create a screen for product form

---
+++ lib/screens/product_form_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ProductForm extends StatefulWidget {
+  @override
+  _ProductFormState createState() => _ProductFormState();
+}
+
+class _ProductFormState extends State<ProductForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Formulário de Produtos'),
+      ),
+    );
+  }
+}

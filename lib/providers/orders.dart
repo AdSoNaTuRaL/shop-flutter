6ffc0f6868ea81447f322b9759d71803928ff525
adsonatural <adsonhenriquesilva@gmail.com>
2021-03-22 16:01:03 +0100

    refactor orders to save information on firebase

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:shop/config.dart';
 import 'package:shop/providers/cart.dart';
 
 class Order {
@@ -28,14 +31,31 @@ class Orders with ChangeNotifier {
     return _items.length;
   }
 
-  void addOrder(List<CartItem> products, double total) {
+  Future<void> addOrder(Cart cart) async {
+    final date = DateTime.now();
+
+    final response = await http.post(
+      '$BASE_URL/orders.json',
+      body: json.encode({
+        'amount': cart.totalAmount,
+        'date': date.toIso8601String(),
+        'products': cart.items.values.map((cartItem) => {
+          'id': cartItem.id,
+          'productId': cartItem.productId,
+          'title': cartItem.title,
+          'quantity': cartItem.quantity,
+          'price': cartItem.price,
+        }).toList(),
+      }),
+    );
+
     _items.insert(
       0,
       Order(
-        id: Random().nextDouble().toString(),
-        amount: total,
-        date: DateTime.now(),
-        products: products,
+        id: json.decode(response.body)['name'],
+        amount: cart.totalAmount,
+        date: date,
+        products: cart.items.values.toList(),
       ),
     );
 

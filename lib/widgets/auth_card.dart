059d95d8643eda56472f17c37b8e92075ade070d
adsonatural <adsonhenriquesilva@gmail.com>
2021-03-23 11:48:43 +0100

    create form for authentication and validations

---
+++ lib/widgets/auth_card.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/exceptions/firebase_exception.dart';
+import 'package:shop/providers/auth.dart';
+
+enum AuthMode { SignUp, SignIn }
+
+class AuthCard extends StatefulWidget {
+  @override
+  _AuthCardState createState() => _AuthCardState();
+}
+
+class _AuthCardState extends State<AuthCard> {
+  bool _isLoading = false;
+  AuthMode _authMode = AuthMode.SignIn;
+  GlobalKey<FormState> _form = GlobalKey();
+
+  final _passwordController = TextEditingController();
+  final Map<String, String> _authData = {
+    'email': '',
+    'password': '',
+  };
+
+  void _showErrorDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: Text('Erro'),
+        content: Text(message),
+        actions: [
+          FlatButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: Text('Fechar'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _submit() async {
+    if (!_form.currentState.validate()) {
+      return;
+    }
+
+    setState(() {
+      _isLoading = true;
+    });
+
+    _form.currentState.save();
+
+    Auth auth = Provider.of(context, listen: false);
+
+    try {
+      if (_authMode == AuthMode.SignIn) {
+        await auth.signIn(_authData['email'], _authData['password']);
+      } else {
+        await auth.signUp(_authData['email'], _authData['password']);
+      }
+    } on FirebaseException catch (error) {
+      _showErrorDialog(error.toString());
+    } catch (error) {
+      _showErrorDialog('Ocorreu um erro inesperado');
+    }
+
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  void _switchAuthMode() {
+    if (_authMode == AuthMode.SignIn) {
+      setState(() {
+        _authMode = AuthMode.SignUp;
+      });
+    } else {
+      setState(() {
+        _authMode = AuthMode.SignIn;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final deviceSize = MediaQuery.of(context).size;
+
+    return Card(
+      elevation: 8.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Container(
+        width: deviceSize.width * 0.75,
+        padding: EdgeInsets.all(16.0),
+        height: _authMode == AuthMode.SignIn ? 290 : 371,
+        child: Form(
+          key: _form,
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(labelText: 'E-mail'),
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) {
+                  if (value.isEmpty || !value.contains('@')) {
+                    return 'Informe um e-mail válido';
+                  }
+                  return null;
+                },
+                onSaved: (value) => _authData['email'] = value,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Senha'),
+                controller: _passwordController,
+                obscureText: true,
+                validator: (value) {
+                  if (value.isEmpty || value.length < 5) {
+                    return 'Informe uma senha válida';
+                  }
+                  return null;
+                },
+                onSaved: (value) => _authData['password'] = value,
+              ),
+              if (_authMode == AuthMode.SignUp)
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Confirmar Senha'),
+                  obscureText: true,
+                  validator: _authMode == AuthMode.SignUp
+                      ? (value) {
+                          if (value != _passwordController.text) {
+                            return 'Senhas não coincidem';
+                          }
+                          return null;
+                        }
+                      : null,
+                ),
+              Spacer(),
+              if (_isLoading)
+                CircularProgressIndicator()
+              else
+                RaisedButton(
+                  onPressed: _submit,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  color: Theme.of(context).primaryColor,
+                  textColor: Theme.of(context).primaryTextTheme.button.color,
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 8),
+                  child: Text(
+                    _authMode == AuthMode.SignIn ? 'ENTRAR' : 'REGISTRAR',
+                  ),
+                ),
+              FlatButton(
+                onPressed: _switchAuthMode,
+                child: Text(_authMode == AuthMode.SignIn
+                    ? 'Criar uma conta'
+                    : 'Já possui uma conta?'),
+                textColor: Theme.of(context).primaryColor,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

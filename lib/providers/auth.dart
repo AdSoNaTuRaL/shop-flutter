5b89a519f35b35a5aa19ab2d925bfa9e86960def
adsonatural <adsonhenriquesilva@gmail.com>
2021-03-23 12:04:10 +0100

    create new getters for token and expire date from firebase

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -6,6 +6,23 @@ import 'package:shop/config.dart';
 import 'package:shop/exceptions/firebase_exception.dart';
 
 class Auth with ChangeNotifier {
+  String _token;
+  DateTime _expireDate;
+
+  bool get isAuth {
+    return token != null;
+  }
+
+  String get token {
+    if (_token != null &&
+        _expireDate != null &&
+        _expireDate.isAfter(DateTime.now())) {
+      return _token;
+    } else {
+      return null;
+    }
+  }
+
   Future<void> signUp(String email, String password) async {
     final response = await http.post(
       BASE_URL_AUTH_SIGNUP,
@@ -19,6 +36,15 @@ class Auth with ChangeNotifier {
     final responseBody = json.decode(response.body);
     if (responseBody['error'] != null) {
       throw FirebaseException(responseBody['error']['message']);
+    } else {
+      _token = responseBody['idToken'];
+      _expireDate = DateTime.now().add(
+        Duration(
+          seconds: int.parse(responseBody['expiresIn']),
+        ),
+      );
+
+      notifyListeners();
     }
 
     return Future.value();
@@ -37,6 +63,15 @@ class Auth with ChangeNotifier {
     final responseBody = json.decode(response.body);
     if (responseBody['error'] != null) {
       throw FirebaseException(responseBody['error']['message']);
+    } else {
+      _token = responseBody['idToken'];
+      _expireDate = DateTime.now().add(
+        Duration(
+          seconds: int.parse(responseBody['expiresIn']),
+        ),
+      );
+
+      notifyListeners();
     }
 
     return Future.value();

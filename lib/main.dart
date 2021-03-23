c592074819084973358a3c98ba6843795727290d
adsonatural <adsonhenriquesilva@gmail.com>
2021-03-23 12:03:33 +0100

    use new screen (authorhome) as new home route

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,11 @@ import 'package:shop/providers/auth.dart';
 import 'package:shop/providers/cart.dart';
 import 'package:shop/providers/orders.dart';
 import 'package:shop/providers/products.dart';
-import 'package:shop/screens/auth_screen.dart';
+import 'package:shop/screens/auth_home_screen.dart';
 import 'package:shop/screens/cart_screen.dart';
 import 'package:shop/screens/orders_screen.dart';
 import 'package:shop/screens/product_detail_screen.dart';
 import 'package:shop/screens/product_form_screen.dart';
-import 'package:shop/screens/products_overview_screen.dart';
 import 'package:shop/screens/products_screen.dart';
 import 'package:shop/utils/app_routes.dart';
 
@@ -43,8 +42,7 @@ class MyApp extends StatelessWidget {
         ),
         // home: ProductOverviewScreen(),
         routes: {
-          AppRoutes.auth: (ctx) => AuthScreen(),
-          AppRoutes.home: (ctx) => ProductOverviewScreen(),
+          AppRoutes.authHome: (ctx) => AuthOrHomeScreen(),
           AppRoutes.cart: (ctx) => CartScreen(),
           AppRoutes.orders: (ctx) => OrdersScreen(),
           AppRoutes.products: (ctx) => ProductsScreen(),
